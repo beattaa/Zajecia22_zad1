aebd11e82b1fd9380f030e6c6b352e1554b93c23
beata <beata_sadek@o2.pl>
2020-06-27 10:50:58 +0200

    first commit

---
+++ src/main/java/pl/beata/zajecia22_zad1/Zajecia22Zad1Application.java
@@ -0,0 +1,13 @@
+package pl.beata.zajecia22_zad1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zajecia22Zad1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zajecia22Zad1Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/beata/zajecia22_zad1/Zajecia22Zad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.beata.zajecia22_zad1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zajecia22Zad1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
